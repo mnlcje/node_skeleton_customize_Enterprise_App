d8995b811d1ed701f564d9784d562711c348a076
mnkcje <mnkcje2050@gmail.com>
2019-08-02 15:28:26 +0530

    Create IActions.ts

---
+++ src/entities/IActions.ts
@@ -0,0 +1,9 @@
+export interface IActions {
+    Type: number;
+    Status: number;
+    MaterialNo: string;
+    PlantID: string;
+    Keys1: string;
+    CreatedOn: Date;
+    CreatedBy: string;
+}
