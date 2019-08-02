dd4fddcdbef6cbfd09eeb46c6c858352c071e1e8
mnkcje <mnkcje2050@gmail.com>
2019-08-02 15:29:43 +0530

    Create IBlockStockSpecificMaterial.ts

---
+++ src/entities/IBlockStockSpecificMaterial.ts
@@ -0,0 +1,9 @@
+export interface BlockStockSpecificMaterial {
+    PlantID?: string;
+    BatchNo?: string;
+    HeaderMaterialNo?: string;
+    MaterialNo: string;
+    Type: string;
+    CreatedBy?: string;
+    CreatedOn?: Date;
+}
