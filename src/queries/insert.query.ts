c858f4349df228d68569096c13804ced05c11452
mnkcje <mnkcje2050@gmail.com>
2019-08-02 15:45:49 +0530

    Update insert.query.ts

--- src/queries/insert.query.ts
+++ src/queries/insert.query.ts
@@ -1,4 +1,6 @@
 export const InsertQuery={
     insertComments:`INSERT INTO InternalComments (PlantID, MaterialNo, Comment, CreatedBy, CreatedOn) VALUES ( :PlantID, :MaterialNo, :Comment, :CreatedBy,  NOW());`
-    
+     insertBlockStock: `INSERT INTO QC_BlockStock (PlantID, MaterialNo, BatchNo, RootCause, Action, BlendPercentage, BlendRestriction, BatchText, CreatedBy, CreatedOn ) VALUES (:PlantID, :MaterialNo, :BatchNo, :RootCause, :Action, :BlendPercentage, :BlendRestriction, :BatchText, :CreatedBy, now());`,
+    insertBlockStockMaterialDetails: `INSERT INTO QC_BlockStockSpecificMaterial (PlantID,BatchNo,HeaderMaterialNo, MaterialNo, Type, CreatedBy, CreatedOn ) VALUES (:PlantID,:BatchNo,:HeaderMaterialNo, :MaterialNo, :Type, :CreatedBy, now());`
+
 }
