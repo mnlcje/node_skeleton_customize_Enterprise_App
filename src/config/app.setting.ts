a7410629923f0e5e915e61fcc82aa1cf24c76835
mnkcje <mnkcje2050@gmail.com>
2019-11-11 13:18:24 +0530

    Update app.setting.ts

--- src/config/app.setting.ts
+++ src/config/app.setting.ts
@@ -5,6 +5,8 @@ import{IConfig} from './IConfig';
 */
 export class AppSetting{
 
+    public static Env = process.env.OFLOW_ENV; //OFLOW_ENV defined in nodeman json files.
+    
     public static getConfig() : IConfig{
         let configManager = new ConfigManager();
         return configManager.Config;
