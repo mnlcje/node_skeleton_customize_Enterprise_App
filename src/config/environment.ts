8a9a67965d044583a7874f988127c5bb513ddccb
mnkcje <mnkcje2050@gmail.com>
2019-11-11 12:49:01 +0530

    Create environment.ts

---
+++ src/config/environment.ts
@@ -0,0 +1,6 @@
+export class Environment {
+    public static readonly DEV = 'dev';
+    public static readonly TESTING = 'test';
+    public static readonly QAS = 'qas';
+    public static readonly PROD = 'prod';
+}
