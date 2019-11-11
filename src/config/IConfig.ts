d626e95277e11a15c3e919dfe0a5692060a77db0
mnkcje <mnkcje2050@gmail.com>
2019-11-11 12:42:43 +0530

    Update IConfig.ts

--- src/config/IConfig.ts
+++ src/config/IConfig.ts
@@ -1,6 +1,19 @@
-export interface IConfig{
-    User:any;
-    DBConnections : any;
-    Port:number;
+export interface IConfig {
+    Port: number;
+    FromMail: string;
+    DefaultToEmail: string;
+    SiteUrl: string;
+    Url: string;
+    DBConnections: any;
+    AppConfig: any;
+    AppSettings: any;
+    EntConfig: any;
+    MDR: any;
     SAP: any;
+    SPConfig: any;
+    OktaConfig: any;
+    ElasticSearch: any;
+    SQSMessageConfig: any;
+    SMTPHost: string;
+    WorkFlow: any;
 }
